e4afe0c5408254dd15457208a11be257757480a2
나범수 <123361606+pass98@users.noreply.github.com>
2023-09-06 18:44:02 +0900

    라우터 추가내용
    라우터 추가내용

--- 09.react_router/src/App.js
+++ 09.react_router/src/App.js
@@ -14,7 +14,8 @@ function App() {
       <Route path="/product" element = {<Porduct/>} />
 
       {/* URL 파라미터 형식으로 Route 구성하기*/ }
-      <Route path = '/product/:pro_no' element={<ProductDetail/>} />
+      /*값을 일일이 쳐야 하기 때문에 정보가 많을 수록 검색하기가 더어려움.*/ 
+      <Route path = '/product/:pro_no/' element={<ProductDetail/>} />
 
       <Route path = '/productDetail2' element={<ProductDetail2/>}/>
     </Routes>
--- 09.react_router/src/components/ProductDetail2.jsx
+++ 09.react_router/src/components/ProductDetail2.jsx
@@ -7,7 +7,10 @@ const ProductDetail2 = () => {
 
 
     //쿼리스트링으로 넘겨받은 데이터를 가져오는 함수 : query.get(name)
-    console.log('Product페이지로부터 전달받은 값 :', query)
+    // ex) http://localhost:3000/product?cate=shirt
+    //      cate에 연결된 값 접근방법 -> query.get('cate') ==> shirt 
+
+    console.log('Product페이지로부터 전달받은 값 :', query.get('cate'))
 
   return (
     <div>ProductDetail2</div>
