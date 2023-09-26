e4c08a38848d2527f9c1e0340e672840c12e95b3
나범수 <123361606+pass98@users.noreply.github.com>
2023-09-26 17:53:45 +0900

    ㅇㄹㅇ
    ㅇㄹㅇㄹ

--- netflix-app/src/components/MovieCard.jsx
+++ netflix-app/src/components/MovieCard.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 const MovieCard = ({movieCard}) => {
   console.log('카드 출력완료', movieCard)
+  
   return (
     <div>
-      
+      <p>
+      {movieCard.overview}
+      <img src='${movieCard.poster_path}'></img>
+      </p>
     </div>
   )
 }
